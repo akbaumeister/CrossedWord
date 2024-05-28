59590a6125672b2d5ea0db3c262d5fd9b25dbbc4
Severin <schmisev@gmail.com>
2024-05-28 14:52:41 +0200

    Find and replace values

--- riddle.js
+++ riddle.js
@@ -13,7 +13,7 @@ var riddle_file = loadFile("all_riddles.csv");
 var riddle_array;
 
 // Parse local CSV file
-Papa.parse(all_riddles, {
+Papa.parse(riddle_file, {
 	complete: function(results) {
 		riddle_array = results.data;
 	}
@@ -21,6 +21,40 @@ Papa.parse(all_riddles, {
 
 console.log("Loaded riddles:", riddle_array);
 
-function clickMe() {
-    console.log("Clicked!");
+var date;
+var hints;
+var clues;
+var solution;
+var second_hints;
+
+function loadRiddle() {
+	var n = Math.floor(Math.random() * (riddle_array.length / 4))
+
+	date = riddle_array[n*4][0];
+	hints = riddle_array[n*4+1];
+	clues = riddle_array[n*4+2].slice(0, 4);
+	solution = riddle_array[n*4+2][4];
+	second_hints = riddle_array[n*4+3];
+
+	console.log(
+		"Current riddle",
+		date,
+		hints,
+		clues,
+		solution,
+		second_hints
+	)
+
+	document.getElementById("yellow-hint").innerHTML = hints[0];
+	document.getElementById("blue-hint").innerHTML = hints[1];
+	document.getElementById("green-hint").innerHTML = hints[2];
+	document.getElementById("purple-hint").innerHTML = hints[3];
+	document.getElementById("center-hint").innerHTML = hints[4];
+
+	document.getElementById("yellow-clue").innerHTML = clues[0];
+	document.getElementById("blue-clue").innerHTML = clues[1];
+	document.getElementById("green-clue").innerHTML = clues[2];
+	document.getElementById("purple-clue").innerHTML = clues[3];
 }
+
+loadRiddle();
