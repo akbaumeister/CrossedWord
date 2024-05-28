af1078b10fe2c0c71b0f15c4bd9bc70189e5ff75
Severin Schmid <36186652+schmisev@users.noreply.github.com>
2024-05-28 11:35:14 +0000

    CSV test

--- riddle.js
+++ riddle.js
@@ -1,3 +1,10 @@
+// Parse local CSV file
+Papa.parse(File.open("all_riddles.csv"), {
+	complete: function(results) {
+		console.log("Finished:", results.data);
+	}
+});
+
 function clickMe() {
     console.log("Clicked!");
 }
